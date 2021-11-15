bcef30a90e77140fb2ca22bb90b94f440bb2f72c
Vinayak Maithani <vinayak@thesolarlabs.com>
2021-11-16 01:40:27 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require ('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const authRoutes = require('./routes/authRoutes');
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoDBUri);
+
+console.log("hi")
 
 const app = express();
 
-app.get('/' , (req , res) => {
-    res.send({ hi: 'there' });
-});
+authRoutes(app);
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
 
