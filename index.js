ffc71e9cc52d5da4df2afd57647d55872a549899
Vinayak Maithani <vinayak@thesolarlabs.com>
2021-11-18 12:17:20 +0530

    google oauth inprogress

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require ('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
 require('./models/user');
@@ -7,15 +9,23 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoDBUri);
 
-console.log("hi")
-
 const app = express();
 
-authRoutes(app);
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey],
+    })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+console.log("hi");
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
-
 
